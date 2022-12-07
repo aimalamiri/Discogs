8e7645069d6a77d76551d83d3a50280811d804db
Aimal Amiri <meaimal@hotmail.com>
2022-12-07 17:33:44 +0430

    Add album validation and tests

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,7 @@
 class Album < ApplicationRecord
+  belongs_to :artist
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :year, presence: true, length: { minimum: 4, maximum: 4 }
+  validates :image_url, length: { maximum: 255 }
+  validates :artist_id, presence: true
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Album Model' do
+    subject { Album.new(title: 'Test Album', image_url: 'https://something.com/image.jpg', year: '2020', artist: Artist.create(name: 'someone')) }
+    before { subject.save }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid without year' do
+      subject.year = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid when the image is longer the 255 characters' do
+      subject.image_url = 'https://something.com/image.jpg' * 10
+      expect(subject).to_not be_valid
+    end
+  end
 end
